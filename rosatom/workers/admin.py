884fb2a8aa149da26a19b63a606b26479f8c258d
ash1k-dev <ash1kekb@yandex.ru>
2024-05-08 18:44:15 +0500

    feat: add models and settings for db

--- rosatom/workers/admin.py
+++ rosatom/workers/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
-# Register your models here.
+from .models import Employee, Position
+
+
+@register(Employee)
+class EmployeeAdmin(ModelAdmin):
+    list_display = ("name", "sex", "position", "birth_date")
+    list_filter = ("sex", "position", "position__category")
+
+
+@register(Position)
+class PositionAdmin(ModelAdmin):
+    list_display = ("name", "category")
+    list_filter = ("name", "category")
--- rosatom/workers/models.py
+++ rosatom/workers/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+ROLE = (
+    ("Руководитель", "Руководитель"),
+    ("Специалист", "Специалист"),
+    ("Служащий", "Служащий"),
+    ("Рабочий", "Рабочий"),
+)
+
+SEX = (
+    ("Мужчина", "Мужчина"),
+    ("Женщина", "Женщина"),
+)
+
+class Employee(models.Model):
+    """Модель сотрудника"""
+
+    name = models.CharField(max_length=100, verbose_name="ФИО")
+    sex = models.CharField(choices=SEX, max_length=100, verbose_name="Пол")
+    position = models.ForeignKey("Position", max_length=100, verbose_name="Должность")
+    birth_date = models.DateField(verbose_name="Дата рождения")
+
+    class Meta:
+        verbose_name = "Сотрудник"
+        verbose_name_plural = "Сотрудники"
+        unique_together = (("name", "birth_date",),)
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    """Модель должности"""
+
+    name = models.CharField(max_length=100, verbose_name="Должность", unique=True)
+    category = models.CharField(choices=ROLE, max_length=100, verbose_name="Категория")
+
+    class Meta:
+        verbose_name = "Должность"
+        verbose_name_plural = "Должности"
+
+    def __str__(self):
+        return self.name
